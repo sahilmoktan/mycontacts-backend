bf035c349f5679536a410e77c413fe9c95d7f8d0
Sahil Moktan <moktanshail9@gmail.com>
2023-05-25 21:49:20 +0545

    update on mongoose, crud operations

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,44 +1,74 @@
+const asyncHandler = require("express-async-handler");
+const Contact = require("../models/contactModel");
 //@desc Get all contacts
 // @route GET / api/contacts
 //@access public
-const getContacts = (req, res) => {
-  res.status(200).json({ message: "Get all contacs" });
-};
+const getContacts = asyncHandler(async (req, res) => {
+  const contacts = await Contact.find();
+  res.status(200).json(contacts);
+});
 
 //@desc Create New contacts
 // @route POST / api/contacts
 //@access public
-const createtContact = (req, res) => {
-    console.log("The request body is :", req.body)
-    const {name, email, phone} = req.body
-    if (!name || !email|| !phone) {
-        res.status(400)
-        throw new Error ("All fields are mandatory")
-    }
-  res.status(201).json({ message: "Created contact" });
-};
+const createtContact = asyncHandler(async (req, res) => {
+  console.log("The request body is :", req.body);
+  const { name, email, phone } = req.body;
+  if (!name || !email || !phone) {
+    res.status(400);
+    throw new Error("All fields are mandatory");
+  }
+  const contact = await Contact.create({
+    name,
+    email,
+    phone,
+  });
+  res.status(201).json(contact);
+});
 
 //@desc Get a contact
 // @route GET / api/contacts
 //@access public
 
-const getContact = (req, res) => {
-  res.status(201).json({ message: `Get a contac for ${req.params.id}` });
-};
+const getContact = asyncHandler(async (req, res) => {
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact not found");
+  }
+  res.status(200).json(contact);
+});
 
 //@desc Update contact
 // @route PUT / api/contacts
 //@access public
-const updateContact = (req, res) => {
-  res.status(200).json({ message: `Update contact for ${req.params.id}` });
-};
+const updateContact = asyncHandler(async (req, res) => {
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact not found");
+  }
+  const updatedContact = await Contact.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+  res.status(200).json(updatedContact);
+});
 
 //@desc Delete  New contacts
 // @route DELETE / api/contacts
 //@access public
-const deleteContact = (req, res) => {
-  res.status(200).json({ message: `delete contact ${req.params.id}` });
-};
+const deleteContact = asyncHandler(async (req, res) => {
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact not found");
+  }
+  console.log("i am hit3");
+  await Contact.findByIdAndRemove();
+  res.status(200).json(contact);
+});
 
 module.exports = {
   getContacts,
--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-const express = require('express')
-const errorHandler = require("./middleware/errorHandler")
-const dotenv = require('dotenv').config()
+const express = require("express");
+const connectDb = require("./config/dbConnection");
+const errorHandler = require("./middleware/errorHandler");
+const dotenv = require("dotenv").config();
 
-const app = express ()
+connectDb();
+const app = express();
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-app.use(express.json())
-app.use("/api/contacts", require("./routes/contactRoutes"))
-app.use(errorHandler)
+app.use(express.json());
+app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use(errorHandler);
 
-
-app.listen(port, ()=>{
-    console.log(`server running on port ${port}`)
- })
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
+});
