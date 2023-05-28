484c554c0c92b26268f9124ca8898a2b63c70e28
Sahil Moktan <moktanshail9@gmail.com>
2023-05-28 11:45:04 +0545

    jwt access token generated successfully

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 
 //@desc Register a user
@@ -11,11 +12,11 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("all fields are mandatory!");
   }
-  // const userAvailable = await User.findOne({ email });
-  // if (userAvailable) {
-  //   res.status(400);
-  //   throw new Error("user already registered!");
-  // }
+  const userAvailable = await User.findOne({ email });
+  if (userAvailable) {
+    res.status(400);
+    throw new Error("user already registered!");
+  }
 
   //hash password
   const hashedPassword = await bcrypt.hash(password, 10);
@@ -36,8 +37,33 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route Post/ api/users/login
 //@access public
 const loginUser = asyncHandler(async (req, res) => {
-  console.log("iam hit");
-  res.json({ message: "login user" });
+  console.log("iam hit login");
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("All fields are Mandatory");
+  }
+  const user = await User.findOne({ email });
+
+  //compare password with hashedpassword
+  console.log("iam hit compare password");
+  if (user && (await bcrypt.compare(password, user.password))) {
+    const accessToken = jwt.sign(
+      {
+        user: {
+          username: user.username,
+          email: user.email,
+          id: user.id,
+        },
+      },
+      process.env.ACCESS_TOKEN_SECERT,
+      { expiresIn: "1m" }
+    );
+    res.status(200).json({ accessToken });
+  } else {
+    res.status("401");
+    throw new Error("email or password is not valied");
+  }
 });
 
 //@desc Current user info
