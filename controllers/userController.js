d4dbc1b9ee36edeca2cdcaca6d6ba06ff5e9e9f9
sahilmoktan <moktanshail9@gmail.com>
2023-11-19 15:36:08 +0545

    mongodb connection success

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,12 +10,12 @@ const registerUser = asyncHandler(async (req, res) => {
   const { username, email, password } = req.body;
   if (!username || !email || !password) {
     res.status(400);
-    throw new Error("all fields are mandatory!");
+    throw new Error("All fields are mandatory!");
   }
   const userAvailable = await User.findOne({ email });
   if (userAvailable) {
     res.status(400);
-    throw new Error("user already registered!");
+    throw new Error("User already registered!");
   }
 
   //hash password
@@ -28,7 +28,7 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(201).json({ _id: user.id, email: user.email });
   } else {
     res.status(400);
-    throw new Error("user data not valied");
+    throw new Error("User Data not valied");
   }
   res.json({ message: "Register the user" });
 });
